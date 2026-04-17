e84c54bc9793833eeee1f3dc6f7c11323f2dc057
adanumar101-byte <adanumar101@gmail.com>
2026-04-17 22:28:27 +0500

    Add files via upload

---
+++ PF(task 1).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+
+    cout<<"Enter value of y: ";
+    cin>>y;
+
+    if(y == 10)
+    {
+        x = 0;
+    }
+    else
+    {
+        x = 1;
+    }
+
+    cout<< "Value of x is: "<<x<<endl;
+
+    return 0;
+}
