d77d28c6538c3b5d77f9332afdd86e318fcf6d34
adanumar101-byte <adanumar101@gmail.com>
2026-04-17 22:30:21 +0500

    Add files via upload

---
+++ PF(task 2).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int temperature;
+
+    cout<<"Enter temperature: ";
+    cin>>temperature;
+
+    if(temperature >= -50 && temperature <= 150)
+    {
+        cout<<"The number is valid"<<endl;
+    }
+
+    return 0;
+}
