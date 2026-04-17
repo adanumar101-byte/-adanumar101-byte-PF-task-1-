757014c83831d0493d42275f7bc8506e18b3c9c6
adanumar101-byte <adanumar101@gmail.com>
2026-04-17 22:31:54 +0500

    Add files via upload

---
+++ PF(task 6).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    inta=2,b=4,c=6;
+
+    if(a == 4 || b > 2)
+        cout<<"A) True"<<endl;
+    else
+        cout<<"A) False"<<endl;
+
+    if(6<=c&&a<3)
+        cout<<"B) True"<<endl;
+    else
+        cout<<"B) False"<<endl;
+
+    if(1!=b&&c!= 3)
+        cout<<"C) True"<<endl;
+    else
+        cout<<"C) False"<<endl;
+
+    if(a>= -1||a<=b)
+        cout<<"D) True"<<endl;
+    else
+        cout<<"D) False"<<endl;
+
+    if(!(a> ))
+        cout<<"E) True"<<endl;
+    else
+        cout<<"E) False"<<endl;
+
+    return 0;
+}
